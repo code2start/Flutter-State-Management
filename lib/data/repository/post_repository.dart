6ed764527fc3e502f9c449af97a5a1e33275eda9
Mohamed Taha <mohamed.taha@litpeaks.com>
2020-07-06 22:20:21 +0200

    lesson 11

---
+++ lib/data/repository/post_repository.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:state_management/data/model/post.dart';
+
+abstract class PostRepository {
+  Future<List<Post>> getPosts();
+}
+
+class PostRepositoryImpl extends PostRepository {
+  @override
+  Future<List<Post>> getPosts() async {
+    final response = await get('https://jsonplaceholder.typicode.com/posts');
+    if (response.statusCode == 200) {
+      final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
+      return parsed.map<Post>((item) => Post.fromJson(item)).toList();
+    } else {
+      throw Exception('Can not load posts');
+    }
+  }
+}
--- lib/ticker.dart
+++
@@ -1,5 +0,0 @@
-class Ticker {
-  Stream<int> tick({int ticks}) => Stream.periodic(Duration(seconds: 1), (x) {
-        return ticks - x - 1;
-      }).take(ticks);
-}
