a7ff6221b4997c9363d9cc0f415ac40a1240e79f
Mohamed Taha <mohamed.taha@litpeaks.com>
2020-07-20 22:35:42 +0200

    lesson18

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:state_management/mycounter.dart';
+import 'package:state_management/next.dart';
 
 main() {
   runApp(MyApp());
@@ -9,8 +10,13 @@ main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Home(),
+    return ChangeNotifierProvider<MyCounter>(
+      create: (BuildContext context) {
+        return MyCounter();
+      },
+      child: MaterialApp(
+        home: Home(),
+      ),
     );
   }
 }
@@ -19,34 +25,44 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Prvider lesson 1'),
-      ),
-      body: ChangeNotifierProvider<MyCounter>(
-        create: (context) => MyCounter(),
-        child: Center(
+        appBar: AppBar(
+          title: Consumer<MyCounter>(builder: (context, mycounter, child) {
+            return Text('${mycounter.counter}');
+          }),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.forward),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Second(),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Consumer<MyCounter>(
-                builder: (context, mycounter, child) {
-                  return Text('${mycounter.counter}');
-                },
-              ),
-              Consumer<MyCounter>(
-                builder: (context, mycounter, child) {
-                  return IconButton(
-                    icon: Icon(Icons.add),
-                    onPressed: () {
-                      mycounter.inc();
-                    },
-                  );
-                },
-              ),
+              Consumer<MyCounter>(builder: (context, mycounter, child) {
+                return Text('${mycounter.counter}');
+              }),
+              Consumer<MyCounter>(builder: (context, mycounter, child) {
+                return IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    mycounter.inc();
+                  },
+                );
+              }),
+              Consumer<MyCounter>(builder: (context, mycounter, child) {
+                return Text('${mycounter.counter}');
+              }),
             ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
