0ea00d11aa50c30d217763202fac01e96a14a9d1
Mohamed Taha <mohamed.taha@litpeaks.com>
2020-07-23 23:34:28 +0200

    lesson 19

--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:state_management/mymodel.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('read extension method'),
+        title: Text('Provider read extension'),
       ),
-      body: Provider(
+      body: ChangeNotifierProvider(
         create: (context) => MyModel('Mohamed Fathy', 22),
         child: Center(
           child: Column(
-            children: <Widget>[MyNameWidget()],
+            children: <Widget>[MyNameWidget(), MyNumber()],
           ),
         ),
       ),
@@ -26,5 +26,25 @@ class MyNameWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     var myModel = Provider.of<MyModel>(context, listen: false);
     return Text(myModel.name);
+  }
+}
+
+class MyNumber extends StatelessWidget {
+  MyNumber({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var m = context.watch<MyModel>();
+    return Column(
+      children: <Widget>[
+        Text('${m.number}'),
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {
+            m.inc();
+          },
+        )
+      ],
+    );
   }
 }
--- lib/mymodel.dart
+++ lib/mymodel.dart
@@ -1,6 +1,12 @@
-class MyModel {
+import 'package:flutter/foundation.dart';
+
+class MyModel with ChangeNotifier {
   String name;
   int number;
-
   MyModel(this.name, this.number);
+
+  inc() {
+    number++;
+    notifyListeners();
+  }
 }
